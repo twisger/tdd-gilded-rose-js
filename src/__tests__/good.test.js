20f94070e5eee8075af31cfac1e8206ef1f69685
Fulin Li <fulin.li@thoughtworks.com>
2020-05-04 12:27:22 +0800

    add good base rule and related test

--- src/__tests__/good.test.js
+++ src/__tests__/good.test.js
@@ -3,3 +3,23 @@ import Good from '../good';
 test('should a good can init', () => {
   expect(new Good({ SellIn: 10, Quality: 10 })).toBeTruthy();
 });
+
+test('should good price change when day change', () => {
+  const good = new Good({ SellIn: 10, Quality: 10 });
+  expect(good.getPrice(1)).not.toEqual(good.getPrice(2));
+});
+
+test('should good price small than 0 and larger than 100', () => {
+  const good = new Good({ SellIn: 10, Quality: 10 });
+  const result = [0, 1, 10, 15].map(day => good.getPrice(day));
+  result.forEach((price) => {
+    expect(price).toBeLessThanOrEqual(50);
+    expect(price).toBeGreaterThanOrEqual(0);
+  });
+});
+
+test('should decrease in double after SellIn', () => {
+  const good = new Good({ SellIn: 5, Quality: 20 });
+  expect(good.getPrice(3)).toBe(17);
+  expect(good.getPrice(8)).toBe(9);
+});
--- src/good.js
+++ src/good.js
@@ -3,4 +3,20 @@ export default class Good {
     this.SellIn = SellIn;
     this.Quality = Quality;
   }
+
+  getPrice(day) {
+    let price;
+    if (day <= this.SellIn) {
+      price = this.Quality - day;
+    } else {
+      price = this.Quality - (day - this.SellIn) * 2 - this.SellIn;
+    }
+    if (price < 0) {
+      return 0;
+    }
+    if (price > 50) {
+      return 50;
+    }
+    return price;
+  }
 }
